f4887e5983e643c38219c875bd0e027343f70b24
vexior27 <jakubmalec07@gmail.com>
2024-10-31 16:50:41 +0100

    updated AddTicket form, by using useForm & zodResolver

--- src/components/AddTicket.js
+++ src/components/AddTicket.js
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { addTicket } from "../api/ticket/addTicket";
-
 import { Input, Textarea, Button, Title } from "@mantine/core";
-
 import { toast } from "react-hot-toast";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ticketSchema } from "../api/ticket/formsValidation/validationSchema";
 
 export default function AddTicket() {
   const navigate = useNavigate();
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(ticketSchema),
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const date = new Date();
-    if (title && description) {
-      addTicket({ title, description, date });
+  const onSubmit = async (data) => {
+    try {
+      await addTicket({ ...data, date: new Date() });
       toast.success("Successfully added new ticket!");
       navigate("/"); // Przekierowanie na stronę główną po dodaniu ticketu
-    } else {
-      toast.error("Tytuł i opis są wymagane!");
+    } catch (error) {
+      toast.error("Failed to add ticket.");
     }
   };
 
@@ -28,21 +33,21 @@ export default function AddTicket() {
       <Title align="center" m="xs">
         Add New Ticket
       </Title>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-1/3">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3 w-1/3">
         <label className="font-medium text-lg flex flex-col gap-1">
           Title
           <Input
             type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            {...register('title')}
+            error={errors.title?.message}
             required
           />
         </label>
         <label className="font-medium text-lg flex flex-col gap-1">
           Description
           <Textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            {...register('description')}
+            error={errors.description?.message}
           />
         </label>
         <Button type="submit" color="green" w={150}>
